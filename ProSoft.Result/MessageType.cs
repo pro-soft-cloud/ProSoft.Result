cba173cbac49e7179e5720add24b8f2ae9f3d385
mirco@home <mircoswinphone@outlook.com>
2026-05-18 10:37:34 +0200

    chore: Intermediate status

---
+++ ProSoft.Result/MessageType.cs
@@ -0,0 +1,27 @@
+﻿namespace ProSoft.Result;
+
+/// <summary>
+/// Enum MessageType
+/// </summary>
+public enum MessageType
+{
+	/// <summary>
+	/// The information
+	/// </summary>
+	Info = 1,
+
+	/// <summary>
+	/// The hint
+	/// </summary>
+	Hint = 2,
+
+	/// <summary>
+	/// The warning
+	/// </summary>
+	Warning = 3,
+
+	/// <summary>
+	/// The error
+	/// </summary>
+	Error = 4
+}
---
+++ ProSoft.Result/ResultStatus.cs
@@ -0,0 +1,22 @@
+﻿namespace ProSoft.Result;
+
+/// <summary>
+/// Enum ResultStatus
+/// </summary>
+public enum ResultStatus
+{
+	/// <summary>
+	/// The success
+	/// </summary>
+	Success = 1,
+
+	/// <summary>
+	/// The partial success
+	/// </summary>
+	PartialSuccess = 2,
+
+	/// <summary>
+	/// The failure
+	/// </summary>
+	Failure = 3
+}
